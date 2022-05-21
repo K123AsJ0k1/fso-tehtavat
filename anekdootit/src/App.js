dbd6bbbf9765447e7efdbc4a4c6d7b0fad406825
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-05-21 13:44:16 +0300

    Tehtävän 1.14 valmisteltu

--- anekdootit/src/App.js
+++ anekdootit/src/App.js
@@ -1,10 +1,51 @@
 import { useState } from 'react'
 
+const Heading = (props) => {
+  return(
+    <h1>{props.text}</h1>
+  )
+}
+
 const Button = (props) => {
   return(
     <button onClick={props.handleClick}>
       {props.text}
     </button>
+  )
+}
+
+const MostVotes = (props) => {
+  let index = 0
+  let value = props.points[0]
+  let final_index = 0
+  let all_zeros = true
+  
+  props.points.forEach(num => {
+    if (num > 0) {
+      all_zeros = false
+    }
+    if (num > value) {
+      value = num
+      final_index = index
+    }
+    index += 1
+  })
+  
+  if (all_zeros) {
+    return(
+      <div>
+        No votes yet.
+      </div>
+    )
+  }
+  
+  return (
+    <div>
+      {props.anecdotes[final_index]}
+      <br/>
+      has {props.points[final_index]} votes
+      <br/>
+    </div>
   )
 }
 
@@ -18,18 +59,32 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
-  const [selected, setSelected] = useState(0)
+
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+ 
+  const [selected, setSelected] = useState(Math.floor(Math.random()*anecdotes.length))
 
   const randomNumber = () => {
     setSelected(Math.floor(Math.random()*anecdotes.length))
   }
 
+  const increasePoint = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
   return (
     <div>
+      <Heading text = "Anecdote of the day" />
       {anecdotes[selected]}
       <br/>
+      has {points[selected]} votes
+      <br/>
+      <Button handleClick = {increasePoint} text = "vote"/>
       <Button handleClick = {randomNumber} text = "next anecdote"/>
+      <Heading text = "Anecdote with most votes" />
+      <MostVotes anecdotes = {anecdotes} points = {points}/>
     </div>
   )
 }
