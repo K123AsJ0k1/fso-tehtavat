f2757901229ed0f1fd99f7fede53dd19da726905
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-05-21 17:46:52 +0300

    Tehtävän 2.4 valmistuminen

--- kurssitiedot/src/App.js
+++ kurssitiedot/src/App.js
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.text}</h1>
+      <h2>{props.text}</h2>
     </div>
   )
 }
@@ -31,7 +31,7 @@ const Total = (props) => {
   
   return(
     <div>
-      <b>Number of exercises {total}</b>
+      <b>total of exercises {total}</b>
     </div>
   )
 }
@@ -45,38 +45,68 @@ const Course = (props) => {
     </div>
   )
 }
+
+const Courses = (props) => {
+  const elements = props.courses.map(course => 
+    <Course key = {course.id} course = {course} />
+    )
+  return(
+    <div>
+      {elements}
+    </div>
+  )
+}
  
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <Courses courses = {courses} />
     </div>
   )
 }
