be24a5a9cc45c4ffcd5dbbb3d89ca7d904431c07
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-05-22 16:02:08 +0300

    Tehtävän 2.10 valmistuminen

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,44 +1,69 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  
-  const [newName, setNewName] = useState('')
+const Filter = (props) => {
+  const handleFilterChange = (event) => {
+    props.setNewFilter(event.target.value)
+  }
 
-  const [newNumber, setNewNumber] = useState('')
+  return (
+    <div>
+      filter show with
+      <input onChange = {handleFilterChange}/>
+    </div>
+  )
+}
 
-  const [newFilter, setNewFilter] = useState('')
-  
+const PersonForm = (props) => {
+  const handleNameChange = (event) => {
+    props.setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    props.setNewNumber(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
 
-    const copyPerson = persons.find(person => person.name === newName)
+    const copyPerson = props.persons.find(person => person.name === props.newName)
     
     if (!(copyPerson === undefined)) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${props.newName} is already added to phonebook`)
       return 0
     }
 
     const personObject = {
-      name: newName,
-      number: newNumber,
+      name: props.newName,
+      number: props.newNumber,
     }
 
     if (copyPerson === undefined) {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      props.setPersons(props.persons.concat(personObject))
+      props.setNewName('')
+      props.setNewNumber('')
     } 
 
   }
 
-  const numberList = persons.map(person => 
-    person.name.toLowerCase().includes(newFilter.toLowerCase())
+  return (
+    <div>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value = {props.newName} onChange = {handleNameChange} />
+          <br/>
+          number: <input value = {props.newNumber} onChange = {handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const Persons = (props) => {
+  const numberList = props.persons.map(person => 
+    person.name.toLowerCase().includes(props.newFilter.toLowerCase())
     ? 
     <p key = {person.name}>
       {person.name} {person.number}
@@ -47,39 +72,51 @@ const App = () => {
     <p key = {person.name}/> 
   )
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-  }
-
   return (
     <div>
-      <h2>Phonebook</h2>
-      filter show with
-      <input onChange = {handleFilterChange}/>
-      <h2>add a new </h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value = {newName} onChange = {handleNameChange} />
-          <br/>
-          number: <input value = {newNumber} onChange = {handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
       { numberList }
     </div>
   )
+}
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      <Filter 
+        setNewFilter = {setNewFilter}
+      />
+
+      <h3>add a new </h3>
+
+      <PersonForm 
+        persons = {persons} 
+        setPersons = {setPersons}
+        newName = {newName} 
+        setNewName = {setNewName} 
+        newNumber = {newNumber} 
+        setNewNumber = {setNewNumber}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons 
+        persons = {persons}
+        newFilter = {newFilter}
+        />
+    </div>
+  )
 }
 
 export default App
