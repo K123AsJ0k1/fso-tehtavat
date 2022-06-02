bd8693f11d3a8c3b01d48056abaf6603cdf45eb3
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-06-02 15:31:27 +0300

    Stabili muutos

--- blogilista/index.js
+++ blogilista/index.js
@@ -1,37 +1,29 @@
 //const http = require('http')
-require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
-const morgan = require('morgan')
-//const app = express()
+const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-// eslint-disable-next-line no-unused-vars
-morgan.token('body', (req, res) => {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
-  }
-  return ''
-})
 
-const app = express()
+//const cors = require('cors')
+//const mongoose = require('mongoose')
+//const Blog = require('./models/blog')
+//const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+//const app = express()
 
 // eslint-disable-next-line no-undef
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+/*
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -50,9 +42,24 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+*/
+app.use('/api/blogs', blogsRouter)
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
+
+
+/*
+const app = require('./app') // varsinainen Express-sovellus
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
+*/
