5b2de6a86c90e225036853fdb109d9bf8557ff52
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-06-03 14:08:05 +0300

    Tehtävän 4.4 valmistuminen

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -3,6 +3,27 @@ const dummy = (blogs) => {
   return 1
 }
 
+// eslint-disable-next-line no-unused-vars
+const totalLikes = (blogs) => {
+  let sum = 0
+  let numbers = false
+
+  Object.keys(blogs).forEach(key => {
+    if (key === 'likes') {
+      sum = blogs[key]
+    }
+    if (key === '0') {
+      numbers = true
+    }
+    if (numbers) {
+      sum = sum + blogs[key].likes
+    }
+  })
+
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
