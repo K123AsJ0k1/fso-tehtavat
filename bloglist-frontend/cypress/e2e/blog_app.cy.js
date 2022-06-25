1d4be241646bb29896c96ee7f79911a8d2fcda01
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-06-25 17:00:44 +0300

    Tehtävän 5.20 valmistuminen

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -50,6 +50,24 @@ describe('Blog app', function() {
       cy.get('#create-button').click()
       cy.contains('title author')
     })
+
+    describe('and a blog exists', function() {
+      beforeEach(function() {
+        cy.contains('create new blog').click()
+        cy.get('#title').type('title')
+        cy.get('#author').type('author')
+        cy.get('#url').type('url')
+        cy.get('#create-button').click()
+      })
+
+      it('A blog can be liked', function() {
+        cy.contains('title author')
+        cy.contains('view').click()
+        cy.contains('likes 0')
+        cy.contains('like').click()
+        cy.contains('likes 1')
+      })
+    })
   })
 
 
