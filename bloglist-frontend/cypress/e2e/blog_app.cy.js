1e0d558577f55b0acb2dbfcdb2b161143199de68
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-06-25 15:47:37 +0300

    Tehtävän 5.18 valmistuminen

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,14 @@
+//const { func } = require("prop-types")
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'testUser',
+      username: 'testUser',
+      password: 'testUser'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,4 +17,20 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testUser')
+      cy.get('#password').type('testUser')
+      cy.get('#login-button').click()
+      cy.contains('testUser logged in')
+    })
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testUser')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.contains('wrong username or password')
+    })
+  })
+
 })
