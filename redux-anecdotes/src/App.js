1c760b700acf0bee7bce938a7d20f3a1ae07528d
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-07-04 14:49:16 +0300

    Tehtävän 6.5 valmistuminen

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -16,11 +16,22 @@ const App = () => {
     dispatch(newAnecdote(content))
   }
 
+  const compareVotes = (a,b) => {
+    //console.log(a.props.votes)
+    if (a.props.votes > b.props.votes) {
+      return -1
+    }
+    if (a.props.votes < b.props.votes) {
+      return 1
+    }
+    return 0
+  }
+
   return (
     <div>
       <h2>Anecdotes</h2>
       {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
+        <div key={anecdote.id} votes={anecdote.votes}>
           <div>
             {anecdote.content}
           </div>
@@ -29,7 +40,7 @@ const App = () => {
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ).sort(compareVotes)}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div><input name="anecdote"/></div>
