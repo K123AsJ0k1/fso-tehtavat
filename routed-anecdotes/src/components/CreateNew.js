9a36156b6d9d8c0ab2938c322a9afe01476bd8dc
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-07-10 13:57:52 +0300

    Tehtävän 7.5 valmistuminen

--- routed-anecdotes/src/components/CreateNew.js
+++ routed-anecdotes/src/components/CreateNew.js
@@ -14,24 +14,33 @@ const CreateNew = ({ addNew }) => {
         votes: 0
       })
     }
-  
+
+    const handleReset = (e) => {
+      e.preventDefault()
+      content.reset()
+      author.reset()
+      info.reset()
+    }
+    //type={content.type} value={content.value} onChange={content.onChange}
+    //type={author.type} value={author.value} onChange={author.onChange}
     return (
       <div>
         <h2>create a new anecdote</h2>
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input type={content.type} value={content.value} onChange={content.onChange} />
+            <input  {...content} />
           </div>
           <div>
             author
-            <input type={author.type} value={author.value} onChange={author.onChange} />
+            <input  {...author} />
           </div>
           <div>
             url for more info
-            <input type={info.type} value={info.value} onChange={info.onChange} />
+            <input {...info} />
           </div>
           <button>create</button>
+          <button onClick={handleReset}>reset</button>
         </form>
       </div>
     ) 
