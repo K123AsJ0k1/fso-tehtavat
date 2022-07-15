67966e224b58b3a898e78aae32d2f06729d289d5
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-07-15 11:23:30 +0300

    Tehtävän 7.17 valmistuminen

--- bloglist-frontend/src/components/Navigation.js
+++ bloglist-frontend/src/components/Navigation.js
@@ -1,6 +1,8 @@
 import Notification from "./Notification";
 import { useDispatch } from "react-redux";
 import { clearUserSetup } from "../reducers/userReducer";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigation = ({ user }) => {
   const dispatch = useDispatch();
@@ -11,12 +13,25 @@ const Navigation = ({ user }) => {
 
   return (
     <div>
-      <h2>blogs</h2>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#" as="span">
+              <Link to="/">blogs</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link to="/users">users</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user.name} logged in{" "}
+              <button onClick={handleLogout}>logout</button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <h2>blog app</h2>
       <Notification />
-      <div>
-        <p>{user.username} logged in</p>
-        <button onClick={handleLogout}>logout</button>
-      </div>
     </div>
   );
 };
